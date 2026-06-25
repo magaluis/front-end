122412e343ab2e65904faac1bf57c602358d751f
magaluis <marialuizaoliveirateixeira37@gmail.com>
2026-06-25 13:29:02 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("Página carregada com sucesso!");
+
+document.getElementById("btn-interativo").addEventListener("click", () => {
+  console.log("Botão clicado pelo usuário.");
+  alert("Integração funcionando!");
+});
